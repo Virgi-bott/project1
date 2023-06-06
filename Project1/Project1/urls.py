209a7cf7be3c7e3b1766a8ad21781e5e25731be5
Virginia Bottino <virginia.bottino@gmail.com>
2023-06-06 01:32:19 -0300

    Clase 20

--- Project1/Project1/urls.py
+++ Project1/Project1/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from Project1.views import saludo, bienvenida, probandoTemplate
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', saludo),
-    path('bienvenida/<nombre>', bienvenida),
-    path('probandoTemplate/', probandoTemplate),
+    path("AppCoder/", include("AppCoder.urls")),
 ]
